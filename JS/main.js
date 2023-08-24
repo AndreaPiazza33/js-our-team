aef9854fcf1483223fd6b72123b2c29a0279662b
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-08-24 15:50:00 +0200

    fix: fixed milestone 0

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+//oggetti da inserire
+const firstMember = {
+    name:'Wayne Barnett',
+    role:'Founder & CEO',
+    photo:'wayne-barnett-founder-ceo.jpg'
+}
+
+const secondMember = {
+    name:'Angela Caroll',
+    role:'Chief Editor',
+    photo:'angela-caroll-chief-editor.jpg'
+}
+
+const thirdMember = {
+    name:'Walter Gordon',
+    role:'Office Manager',
+    photo:'walter-gordon-office-manager.jpg'
+}
+
+const fourthMember = {
+    name:'Angela Lopez',
+    role:'Social Media Manager',
+    photo:'angela-lopez-social-media-manager.jpg'
+}
+
+const fifthMember = {
+    name:'Scott Estrada',
+    role:'Developer',
+    photo:'scott-estrada-developer.jpg'
+}
+
+const sixthMember = {
+    name:'Barbara Ramos',
+    role:'Graphic Designer',
+    photo:'barbara-ramos-graphic-designer.jpg'
+}
+
+//contenitore 
+const ourTeam = [firstMember,secondMember,thirdMember,fourthMember,fifthMember,sixthMember]
+
