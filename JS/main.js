cf2fb143ab896581b27919dd98a6e889bada661e
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-08-24 16:27:56 +0200

    feat: add milestone 1

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,67 @@
+//MILESTONE 0
 //oggetti da inserire
 const firstMember = {
     name:'Wayne Barnett',
     role:'Founder & CEO',
     photo:'wayne-barnett-founder-ceo.jpg'
 }
-
 const secondMember = {
     name:'Angela Caroll',
     role:'Chief Editor',
     photo:'angela-caroll-chief-editor.jpg'
 }
-
 const thirdMember = {
     name:'Walter Gordon',
     role:'Office Manager',
     photo:'walter-gordon-office-manager.jpg'
 }
-
 const fourthMember = {
     name:'Angela Lopez',
     role:'Social Media Manager',
     photo:'angela-lopez-social-media-manager.jpg'
 }
-
 const fifthMember = {
     name:'Scott Estrada',
     role:'Developer',
     photo:'scott-estrada-developer.jpg'
 }
-
 const sixthMember = {
     name:'Barbara Ramos',
     role:'Graphic Designer',
     photo:'barbara-ramos-graphic-designer.jpg'
 }
-
 //contenitore 
 const ourTeam = [firstMember,secondMember,thirdMember,fourthMember,fifthMember,sixthMember]
 
+//MILESTONE 1
+//stampa in console
+//primo membro
+console.log(ourTeam[0]);
+console.log('nome:','',ourTeam[0].name);
+console.log('ruolo:','',ourTeam[0].role);
+console.log('foto:','',ourTeam[0].photo);
+//secondo membro
+console.log(ourTeam[1]);
+console.log('nome:','',ourTeam[1].name);
+console.log('ruolo:','',ourTeam[1].role);
+console.log('foto:','',ourTeam[1].photo);
+//terzo membro
+console.log(ourTeam[2]);
+console.log('nome:','',ourTeam[2].name);
+console.log('ruolo:','',ourTeam[2].role);
+console.log('foto:','',ourTeam[2].photo);
+//quarto membro
+console.log(ourTeam[3]);
+console.log('nome:','',ourTeam[3].name);
+console.log('ruolo:','',ourTeam[3].role);
+console.log('foto:','',ourTeam[3].photo);
+//quinto membro
+console.log(ourTeam[4]);
+console.log('nome:','',ourTeam[4].name);
+console.log('ruolo:','',ourTeam[4].role);
+console.log('foto:','',ourTeam[4].photo);
+//sesto membro
+console.log(ourTeam[5]);
+console.log('nome:','',ourTeam[5].name);
+console.log('ruolo:','',ourTeam[5].role);
+console.log('foto:','',ourTeam[5].photo);
