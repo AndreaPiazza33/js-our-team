486b8d9f81353d96c5f8cc5cb5c9b956947b6d15
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-08-24 17:55:55 +0200

    feat: add milestone 2

--- JS/main.js
+++ JS/main.js
@@ -65,3 +65,54 @@ console.log(ourTeam[5]);
 console.log('nome:','',ourTeam[5].name);
 console.log('ruolo:','',ourTeam[5].role);
 console.log('foto:','',ourTeam[5].photo);
+
+//MILESTONE 2
+//prima persona
+const firstName =document.getElementById('first-name')
+firstName.innerText =ourTeam[0].name
+const firstRole =document.getElementById('first-role')
+firstRole.innerText =ourTeam[0].role
+const firstPhoto =document.getElementById('first-photo')
+firstPhoto.innerText =ourTeam[0].photo
+//seconda persona
+const secondName =document.getElementById('second-name')
+secondName.innerText =ourTeam[1].name
+const secondRole =document.getElementById('second-role')
+secondRole.innerText =ourTeam[1].role
+const secondPhoto =document.getElementById('second-photo')
+secondPhoto.innerText =ourTeam[1].photo
+//terza persona
+const thirdName =document.getElementById('third-name')
+thirdName.innerText =ourTeam[2].name
+const thirdRole =document.getElementById('third-role')
+thirdRole.innerText =ourTeam[2].role
+const thirdPhoto =document.getElementById('third-photo')
+thirdPhoto.innerText =ourTeam[2].photo
+//quarta persona
+const fourthName =document.getElementById('fourth-name')
+fourthName.innerText =ourTeam[3].name
+const fourthRole =document.getElementById('fourth-role')
+fourthRole.innerText =ourTeam[3].role
+const fourthPhoto =document.getElementById('fourth-photo')
+fourthPhoto.innerText =ourTeam[3].photo
+//quinta persona
+const fifthName =document.getElementById('fifth-name')
+fifthName.innerText =ourTeam[4].name
+const fifthRole =document.getElementById('fifth-role')
+fifthRole.innerText =ourTeam[4].role
+const fifthPhoto =document.getElementById('fifth-photo')
+fifthPhoto.innerText =ourTeam[4].photo
+//sesta persona
+const sixthName =document.getElementById('sixth-name')
+sixthName.innerText =ourTeam[5].name
+const sixthRole =document.getElementById('sixth-role')
+sixthRole.innerText =ourTeam[5].role
+const sixthPhoto =document.getElementById('sixth-photo')
+sixthPhoto.innerText =ourTeam[5].photo
+
+
+
+
+
+
+
